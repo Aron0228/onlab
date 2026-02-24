e4030386ddccb40956920ed9d04e03a3ad9ab3e3
Aron0228 <aronreszegi2@gmail.com>
2026-02-24 23:25:07 +0100

    Remove development environment file and update API host configuration to use environment variable

--- client/app/adapters/application.ts
+++ client/app/adapters/application.ts
@@ -5,7 +5,7 @@ import { pluralize } from 'ember-inflector';
 const _pluralize = pluralize as (word: string) => string;
 
 export default class ApplicationAdapter extends JSONAPIAdapter {
-  host = 'http://localhost:30022';
+  host = (import.meta.env.API_URL as string) ?? 'http://localhost:30022';
 
   pathForType(modelName: string): string {
     return _pluralize(camelCase(modelName));
