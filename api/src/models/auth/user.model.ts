d4e8e1bda914544795749287a930e45b615c4135
Aron0228 <aronreszegi2@gmail.com>
2026-03-11 19:44:25 +0100

    Add User model

---
+++ api/src/models/auth/user.model.ts
@@ -0,0 +1,52 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model({
+  settings: {
+    forceId: false,
+    postgresql: {schema: 'auth', table: 'user'},
+  },
+})
+export class User extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  id: number;
+
+  @property({
+    type: 'number',
+    required: true,
+    postgresql: {columnName: 'github_id'},
+  })
+  githubId: number;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {columnName: 'username'},
+  })
+  username: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {columnName: 'email'},
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+    postgresql: {columnName: 'avatar_url'},
+  })
+  avatarUrl: string;
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export type UserRelations = object;
+
+export type UserWithRelations = User & UserRelations;
