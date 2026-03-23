e406e596df866368de6415bc9fa7b56ef83c995b
Aron0228 <aronreszegi2@gmail.com>
2026-03-23 20:33:05 +0100

    feat(client): add new GithubRepository, GithubIssue models

---
+++ client/app/models/github-issue.ts
@@ -0,0 +1,14 @@
+import Model, {attr, belongsTo} from '@warp-drive/legacy/model';
+import type GithubRepositoryModel from './github-repository';
+
+export default class GithubIssueModel extends Model {
+  @belongsTo('github-repository', {async: false, inverse: 'issues'})
+  declare repository: GithubRepositoryModel | null;
+
+  @attr('number') declare repositoryId: number;
+  @attr('number') declare githubId: number;
+  @attr('number') declare githubIssueNumber: number;
+  @attr('string') declare title: string;
+  @attr('string') declare status: string;
+  @attr('string') declare description: string | null;
+}
