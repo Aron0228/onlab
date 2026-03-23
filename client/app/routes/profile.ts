3172b5fefba49e197d2b78f7d01ffa806d731fc7
Aron0228 <aronreszegi2@gmail.com>
2026-03-23 20:36:39 +0100

    feat(client): make profile route's routeBack arg optional

--- client/app/routes/profile.ts
+++ client/app/routes/profile.ts
@@ -3,12 +3,18 @@ import type Transition from '@ember/routing/transition';
 
 type ProfileRouteModel = {
   routeBack: string;
+  routeBackUrl: string | null;
 };
 
-export default class ProfileRoute extends Route {
+export default class ProfileRoute extends Route<ProfileRouteModel> {
   model(_: unknown, transition: Transition): ProfileRouteModel {
+    const routeBack = transition.to?.queryParams.routeBack;
+    const routeBackUrl = transition.to?.queryParams.routeBackUrl;
+
     return {
-      routeBack: transition.to?.queryParams.routeBack ?? 'workspaces.index',
+      routeBack:
+        typeof routeBack === 'string' ? routeBack : 'workspaces.index',
+      routeBackUrl: typeof routeBackUrl === 'string' ? routeBackUrl : null,
     };
   }
 }
